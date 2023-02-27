aafcefdb4268fc256a3ed8b5a2d2ecb66a0ce07c
root <ytianhui2004@gmail.com>
2023-02-27 06:58:33 +0000

    ID_S2_EX2

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -73,29 +73,39 @@ def show_range_image(frame, lidar_name):
 
 
 # visualize lidar point-cloud
-def show_pcl(frame, lidar_name):
+def show_pcl(pcl):
     ####### ID_S1_EX2 START #######     
     #######
     print("student task ID_S1_EX2")
+    # 下面部分要改一下!!!!!!!!
     # step 1 : initialize open3d with key callback and create window
-    vis_lpc = o3d.visualization.VisualizerWithKeyCallback()
-    vis_lpc.create_window(window_name='Open3D', width=1920, height=1080, left=10, top=10, visible=True)
-    vis_lpc.register_key_callback(262,right_click)
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window(window_name='Open3D', width=1920, height=1080, left=10, top=10, visible=True)
+
+    global idx
+    idx= True
+    vis.register_key_callback(262,right_click)
+    
     # step 2 : create instance of open3d point-cloud class
+    pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
-
-    # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    pcd.points = o3d.utility.Vector3dVector(pcl[:,:3])
     
-    # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
-
+    # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    vis.add_geometry(pcd)
+    
+    # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)    
+    while idx:
+        vis.poll_events()
+        vis.update_renderer()
     #######
     ####### ID_S1_EX2 END #######     
-    range_image_to_point_cloud(frame, lidar_name)
-
+       
 
 def right_click(vis_lpc):
-   
+    print('right click' + idx)
+    idx = False
     return
 
 
@@ -116,13 +126,19 @@ def bev_from_pcl(lidar_pcl, configs):
     #######
     print("student task ID_S2_EX1")
     ## step 1 : compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    bev_discret = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
 
     ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
-
-    # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
-
-    # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    lidar_pcl_cpy = np.copy(lidar_pcl)
+    lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / bev_discret))
     
+    # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur 
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / bev_discret) + (configs.bev_width + 1) / 2)
+    lidar_pcl_cpy[:, 1] = np.abs(lidar_pcl_cpy[:,1]) #no negative
+    
+    # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    # show_pcl(lidar_pcl_cpy) # remove due to next project
+
     #######
     ####### ID_S2_EX1 END #######     
     
@@ -130,26 +146,42 @@ def bev_from_pcl(lidar_pcl, configs):
     # Compute intensity layer of the BEV map
     ####### ID_S2_EX2 START #######     
     #######
-    print("student task ID_S2_EX2")
-    #pcl_to_bev(lidar_pcl, configs)
-    
+    print("student task ID_S2_EX2")    
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height, configs.bev_width))
 
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
-
+    lidar_pcl_cpy[lidar_pcl_cpy[:,3]>1.0,3] = 1.0
+    idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_top = lidar_pcl_cpy[idx_intensity]
+    
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
-
+    _, idx_height_unique = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True) # _means useless parameter
+    lidar_pcl_top = lidar_pcl_cpy[idx_height_unique]
+    
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    intensity_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 3] / (np.amax(lidar_pcl_top[:, 3])-np.amin(lidar_pcl_top[:, 3]))
 
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    img_intensity = intensity_map * 256
+    img_intensity = img_intensity.astype(np.uint8)
+    while (1):
+            cv2.imshow('img_intensity', img_intensity)
+            if cv2.waitKey(10) & 0xFF == 27:
+                break
+    cv2.destroyAllWindows()
     #######
     ####### ID_S2_EX2 END ####### 
 
 
+    
+    
+    
+    
+    
     # Compute height layer of the BEV map
     ####### ID_S2_EX3 START #######     
     #######
@@ -208,10 +240,7 @@ def bev_from_pcl(lidar_pcl, configs):
     bev_maps = torch.from_numpy(bev_maps)  # create tensor from birds-eye view
     input_bev_maps = bev_maps.to(configs.device, non_blocking=True).float()
     
-    
-    
-
-    
+         
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
     img_height = height_map * 256
     img_height = img_height.astype(np.uint8)
