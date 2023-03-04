precision not correct

loop_over_dataset.py
@@ -84,11 +84,36 @@ camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
-exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = ['show_objects_in_bev_labels_in_camera'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+##----ID_S1_EX1----
+#exec_detection = [] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+#exec_tracking = ['perform_tracking'] # options are 'perform_tracking' 
+#exec_visualization = ['show_tracks', 'make_tracking_movie'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+
+##----ID_S1_EX2----
+# exec_detection = []
+# exec_tracking = []
+# exec_visualization = ['show_pcl']
+
+##----ID_S2_EX1, ID_S2_EX2, & ID_S2_EX3----
+# exec_detection = ['bev_from_pcl'] 
+# exec_tracking = [] 
+# exec_visualization=[]
+
+##----ID_S3_EX1 & ID_S3_EX2----
+# exec_detection = ['bev_from_pcl', 'detect_objects']
+# exec_tracking = []
+# exec_visualization = ['show_objects_in_bev_labels_in_camera']
+
+##----ID_S4_EX1,ID_S4_EX3----
+exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance']
+exec_tracking = []
+exec_visualization = ['show_detection_performance']
+
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0 # set pause time between frames in ms (0 = stop between frames until key is pressed)
+
+
+
 
 
 ##################
@@ -149,10 +174,6 @@ while True:
         else:
             if 'detect_objects' in exec_list:
                 print('detecting objects in lidar pointcloud')
-                
-                print('lidar_bev')
-                print(lidar_bev)
-
                 detections = det.detect_objects(lidar_bev, model_det, configs_det)
             else:
                 print('loading detected objects from result file')
@@ -285,7 +306,7 @@ while True:
 
 ## Evaluate object detection performance
 if 'show_detection_performance' in exec_list:
-    eval.compute_performance_stats(det_performance_all, configs_det)
+    eval.compute_performance_stats(det_performance_all)
 
 ## Plot RMSE for all tracks
 if 'show_tracks' in exec_list:

student/objdet_eval.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 import torch
 from shapely.geometry import Polygon
 from operator import itemgetter
+from easydict import EasyDict as edict
 
 # add project directory to python path to enable relative imports
 import os
@@ -96,13 +97,17 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
     
     ## step 1 : compute the total number of positives present in the scene
     all_positives = 0
+    all_positives = labels_valid.sum()
 
     ## step 2 : compute the number of false negatives
     false_negatives = 0
-
+    true_positives=len(ious)
+    false_negatives = all_positives - true_positives
+    
     ## step 3 : compute the number of false positives
     false_positives = 0
-    
+    false_positives = len(detections) - true_positives
+
     #######
     ####### ID_S4_EX2 END #######     
     
@@ -123,19 +128,26 @@ def compute_performance_stats(det_performance_all):
         ious.append(item[0])
         center_devs.append(item[1])
         pos_negs.append(item[2])
-    
+        pos_negs_arr = np.asarray(pos_negs)
+
     ####### ID_S4_EX3 START #######     
     #######    
     print('student task ID_S4_EX3')
 
     ## step 1 : extract the total number of positives, true positives, false negatives and false positives
+    positives = sum(pos_negs_arr[:,0])
+    true_positives = sum(pos_negs_arr[:,1])
+    false_negatives = sum(pos_negs_arr[:,2])
+    false_positives = sum(pos_negs_arr[:,3])
     
     ## step 2 : compute precision
     precision = 0.0
-
+    precision = true_positives /float(true_positives + false_positives)   
+    
     ## step 3 : compute recall 
     recall = 0.0
-
+    recall = true_positives / float(true_positives + false_negatives)    
+    
     #######    
     ####### ID_S4_EX3 END #######     
     print('precision = ' + str(precision) + ", recall = " + str(recall))   